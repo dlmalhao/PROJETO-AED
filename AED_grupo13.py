93002b7446e0b708e33138cf7c3e890d1f23f386
dlmalhao <dlmalhao@gmail.com>
2021-01-26 23:52:14 +0000

    Design proviório

--- AED_grupo13.py
+++ AED_grupo13.py
@@ -2,6 +2,9 @@ from tkinter import *
 from tkinter import messagebox
 import os
 import re
+from tkinter import filedialog
+from PIL import ImageTk, Image
+import tkinter.font as font
 
 
 def janelaRegistar():
@@ -57,60 +60,117 @@ def janelaRegistar():
         loginwindow.update()
         loginwindow.deiconify()
 
+
+    def escolhe_imagem():
+        imagem=filedialog.askopenfilename(title="Select file", filetypes=(("jpg files",".jpg"),("png files",".png"),("all files",".")))
+        global img 
+        img = ImageTk.PhotoImage(file = imagem)
+        canvas1.itemconfig(img_id, image=img)
+
+    # Guarda dados no ficheiro perfil.txt
+    #def guardar_setup():
+         #f = open(ficheiro_perfil, "w")
+         #linha = filename      # Imagem d eperfil;tema selecionado
+         #f.write(linha + "\n") 
+         #f.close()
+         #global img
+         # atualiza canvas de imagem de perfil, com imagem guardada em ficheiro
+         #img = ImageTk.PhotoImage(file = filename)
+         #canvas.itemconfig(image_id, image=img)
+
     # /-/-/-/-/-/-/-/-/-/-/-
     # Register Window
     # /-/-/-/-/-/-/-/-/-/-/-
+    
 
     registerWindow = Toplevel(loginwindow)
     registerWindow.title("Register")
     registerWindow.geometry("800x600")
 
+    registerWindow.configure(bg="#D1BEA6")
+
+    #wallpaper registo
+    #wall2=ImageTk.PhotoImage(file="Images/wallpaper6.jpg")
+    #wallpaper2=Label(registerWindow, image=wall)
+    #wallpaper2.place(x=0, y=0,relwidth=1, relheight=1)
+
     loginwindow.withdraw()
+    
+
+    registerlbl_usernamePerfil = Label(registerWindow, text="Username:", fg="black",bg = "#D1BEA6", font=("Microsoft YaHei", 12))
+    registerlbl_usernamePerfil.place(x=270, y=150)
+
+    registertxt_usernamePerfil = Entry(registerWindow,  fg="black", font = ("Microsoft YaHei", 10), width=35, relief="raised")
+    registertxt_usernamePerfil.place(x=270, y=180)
 
     registerlbl_login = Label(
-        registerWindow, text="Register", fg="red", font=("Helvetica", 30))
+        registerWindow, text="Register", fg="black",bg = "#D1BEA6", font=("Microsoft YaHei", 30))
     registerlbl_login.place(x=330, y=80)
 
     registerlbl_username = Label(
-        registerWindow, text="Email:", fg="red", font=("Helvetica", 12))
+        registerWindow, text="Email:", fg="black",bg = "#D1BEA6", font=("Microsoft YaHei", 12))
     registerlbl_username.place(x=270, y=200)
 
     registertxt_username = Entry(registerWindow,  fg="black", font=(
-        "Helvetica", 10), width=35, relief="raised")
+        "Microsoft YaHei", 10), width=35, relief="raised")
     registertxt_username.place(x=270, y=230)
 
     registerlbl_password = Label(
-        registerWindow, text="Password:", fg="red", font=("Helvetica", 12))
+        registerWindow, text="Password:", fg="black",bg = "#D1BEA6", font=("Microsoft YaHei", 12))
     registerlbl_password.place(x=270, y=270)
 
     registertxt_password = Entry(registerWindow, fg="black", font=(
-        "Helvetica", 10), show="*", width=35, relief="raised")
+        "Microsoft YaHei", 10), show="*", width=35, relief="raised")
     registertxt_password.place(x=270, y=300)
 
     registerlbl_confpassword = Label(
-        registerWindow, text="Confirm Password:", fg="red", font=("Helvetica", 12))
+        registerWindow, text="Confirm Password:", fg="black",bg = "#D1BEA6", font=("Microsoft YaHei", 12))
     registerlbl_confpassword.place(x=270, y=340)
 
-    registertxt_confpassword = Entry(registerWindow, fg="black", font=(
-        "Helvetica", 10), show="*", width=35, relief="raised")
+    registertxt_confpassword = Entry(registerWindow, fg="black", font=("Microsoft YaHei", 10), show="*", width=35, relief="raised")
     registertxt_confpassword.place(x=270, y=370)
 
-    registarButtonRegister = Button(registerWindow, text="Register", width="10", font=(
-        "Helvetica", 12), fg="red", command=registo)
+    registarButtonRegister = Button(registerWindow, text="Register", width="10", font=("Microsoft YaHei", 12), fg="black", command=registo)
     registarButtonRegister.place(x=420, y=400)
 
-    menuButton = Button(registerWindow, text="Back", width="10", font=(
-        "Helvetica", 12), fg="red", command=voltar)
+    menuButton = Button(registerWindow, text="Back", width="10", font=("Microsoft YaHei", 12), fg="black", command=voltar)
     menuButton.place(x=270, y=400)
 
+    canvas1 = Canvas(registerWindow, width=100, height=100, bd="2", relief="sunken")
+    canvas1.place(x = 20, y = 20)
+    
+    img=ImageTk.PhotoImage(file= "avatar0.jpg")
+    img_id = canvas1.create_image(0,0,anchor = "nw", image = img)
 
+    button_imagem =Button(registerWindow, text="Selecionar \nFoto de Perfil", width="14", font=("Helvetica", 12), fg="black", command=escolhe_imagem)
+    button_imagem.place(x=18, y=230)
+
+    
 def main_page():
+
+    def escolhe_imagem():
+        imagem=filedialog.askopenfilename(title="Select file", filetypes=(("jpg files",".jpg"),("png files",".png"),("all files",".")))
+        global img 
+        img = ImageTk.PhotoImage(file = imagem)
+        canvas1.itemconfig(img_id, image=img)
+    
+    
 
     loginwindow.withdraw()
 
     window = Toplevel()
     window.geometry("800x600")
     window.title("Home Page")
+
+    #wallpaper home page
+    wall4=ImageTk.PhotoImage(file="Images/wallpaper6.jpg")
+    wallpaper4=Label(window, image=wall4)
+    wallpaper4.place(x=0, y=0,relwidth=1, relheight=1)
+
+    #wallpaper home page-2º fundo
+    #wall5=ImageTk.PhotoImage(file="Images/livro.jpg")
+    #wallpaper5=Label(frame, image=wall5)
+    #wallpaper5.place(x=0, y=0,relwidth=1, relheight=1)
 
     # MENU BAR
     barra_Menu = Menu(window)
@@ -129,14 +189,19 @@ def main_page():
 
     # CONTAINER
 
-    panel1 = PanedWindow(window, width=200, height=100, bd="2", relief="sunken")
+    panel1 = PanedWindow(window, width=200, height=105, bd="2", relief="sunken")
     panel1.pack(fill=Y, side=RIGHT)
 
-    canvas1 = Canvas(panel1, width=150, height=100, bd="2", relief="sunken")
+    canvas1 = Canvas(panel1, width=100, height=100, bd="2", relief="sunken")
     canvas1.pack(side=TOP)
 
-    
+    btn1 = Button(panel1, text = "Select Image", command = escolhe_imagem)
+    btn1.place(x =20, y = 110)
 
+    img=ImageTk.PhotoImage(file= "avatar0.jpg")
+    img_id = canvas1.create_image(0,0,anchor = "nw", image = img)
+    
+    
     window.mainloop()
 
 
@@ -209,7 +274,7 @@ def admin_mode():
 
     panel1 = PanedWindow(window, width=100, height=100,
                          bd="3", relief="sunken")
-    panel1.pack(fill=X, side=TOP)
+    panel1.pack(fill=X, side=TOP) 
 
     user_button = Button(panel1, text="Profile",
                          width=6, height=2, command=user)
@@ -261,6 +326,11 @@ loginwindow = Tk()  # invoca classe tk, cria a "main window"
 loginwindow.geometry("800x600")
 loginwindow.title("Projeto de AED")
 
+#criar wallpaper
+wall=ImageTk.PhotoImage(file="Images/wallpaper6.jpg")
+wallpaper=Label(loginwindow, image=wall)
+wallpaper.place(x=0, y=0,relwidth=1, relheight=1)
+
 original_frame = Frame(loginwindow)
 loginwindow.configure(bg="#DBE2AC")
 
@@ -291,6 +361,7 @@ loginButton.place(x=420, y=400)
 registarButton = Button(loginwindow, text="Register", width="10", font=(
     "Helvetica", 12), fg="red", command=janelaRegistar)
 registarButton.place(x=270, y=400)
+
 
 regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
